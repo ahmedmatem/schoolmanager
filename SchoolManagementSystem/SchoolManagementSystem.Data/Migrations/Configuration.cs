5f1441d18b581444c2b5f55a1a197862e1159b10
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2017-01-29 15:29:07 +0000

    Add migration to the Project

---
+++ SchoolManagementSystem/SchoolManagementSystem.Data/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace SchoolManagementSystem.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<SchoolManagementSystem.Data.ApplicationDbContext>
+    {
+        // TODO: turn next proporties to false in release mode
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(SchoolManagementSystem.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- SchoolManagementSystem/SchoolManagementSystem.Web/Global.asax.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Web/Global.asax.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace SchoolManagementSystem.Web
+﻿namespace SchoolManagementSystem.Web
 {
+    using Data;
+    using Data.Migrations;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
